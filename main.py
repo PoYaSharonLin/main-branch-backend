bf9c4f774447a586623b868ed5141c37f723f4d6
SaviorDT <tony20040424@gmail.com>
2025-03-12 02:08:36 +0800

    Add cors.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter, Request
+from fastapi.middleware.cors import CORSMiddleware
 from routers.post import post_router
 from routers.login import login_router
 from routers.comment import comment_router
@@ -26,5 +27,13 @@ router.include_router(
 app.include_router(router)
 
 app.add_middleware(SessionMiddleware, secret_key=os.getenv('APP_SECRET'))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.middleware("http")(auth_middleware)
